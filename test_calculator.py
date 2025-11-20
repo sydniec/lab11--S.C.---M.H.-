d72e944198d1a261d5a652b39ac515dd8d033ed6
sydniec <sgcolaw01@gmail.com>
2025-11-19 22:35:49 -0500

    "modified test_calculator p1"

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 import math
-from calculator import *
+from lab11.calculator import *
 
 # https://github.com/sydniec/lab11--S.C.---M.H.-
 # Partner 1: sydniec
@@ -15,31 +15,56 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self): # 3 assertions
-        self.assertEqual(sub(5, 3), 2)
-        self.assertEqual(sub(0, 5), -5)
-        self.assertEqual(sub(-2, -3), 1)
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(0, 5), -5)
+        self.assertEqual(subtract(-2, -3), 1)
     # ##########################
 
     ######## Partner 1
-    # partner 1 tests (left for partner)
+    def test_multiply(self):
+        self.assertEqual(multiply(-1, -1), 1)
+        self.assertEqual(multiply(-4, 10), -40)
+        self.assertEqual(multiply(8, 8), 64)
+        self.assertEqual(multiply(18, 0), 0)
+
+    def test_divide(self):
+        self.assertAlmostEqual(divide(-1, 1), -1)
+        self.assertAlmostEqual(divide(-8, 64), -8)
+        self.assertAlmostEqual(divide(4, 0), 0)
+        self.assertAlmostEqual(divide(15, 225), 15)
 
     ######## Partner 2
     def test_divide_by_zero(self): # 1 assertion
         with self.assertRaises(ZeroDivisionError):
-            div(0, 5)
+            divide(0, 5)
 
     def test_logarithm(self): # 3 assertions
-        self.assertAlmostEqual(log(10, 100), 2)
-        self.assertAlmostEqual(log(2, 8), 3)
-        self.assertAlmostEqual(log(math.e, math.e**2), 2)
+        self.assertAlmostEqual(logarithm(10, 100), 2)
+        self.assertAlmostEqual(logarithm(2, 8), 3)
+        self.assertAlmostEqual(logarithm(math.e, math.e**2), 2)
 
     def test_log_invalid_base(self): # 1 assertion
         with self.assertRaises(ValueError):
-            log(1, 10)
+            logarithm(1, 10)
     # ##########################
 
     ######## Partner 1
-    # partner 1 tests (left for partner)
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            logarithm(2, 0)
+
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(hypotenuse(1, 1), math.sqrt(2))
+        self.assertAlmostEqual(hypotenuse(-4, 3), 5)
+        self.assertAlmostEqual(hypotenuse(-3, -3), math.sqrt(18))
+        self.assertAlmostEqual(hypotenuse(2, 3), math.sqrt(13))
+
+    def test_sqrt(self):
+        with self.assertRaises(ValueError):
+            square_root(-4)
+        self.assertEqual(square_root(4), 2)
+        self.assertEqual(square_root(16), 4)
+        self.assertEqual(square_root(225), 15)
 
 # Do not touch this
 if __name__ == "__main__":
